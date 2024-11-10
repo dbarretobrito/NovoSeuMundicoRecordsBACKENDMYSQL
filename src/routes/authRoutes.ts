9c54b1d2aa4e2d50918b93f07aea80e46a6caee2
Daniel Barreto <dbarretobrito@gmail.com>
2024-11-10 18:39:39 -0300

    Configura a rota de autenticação para login de administradores

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -3,6 +3,7 @@ import { loginAdmin } from '../controllers/authController';
 
 const router = Router();
 
+// Define a rota para login de administradores
 router.post('/login', loginAdmin);
 
 export default router;
