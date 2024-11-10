510950197c6cd0cf385fdd389de0a4ce3c5589c8
Daniel Barreto <dbarretobrito@gmail.com>
2024-11-10 18:28:42 -0300

    Define rotas para manipulação e visualização de produtos, com autenticação de admin para operações sensíveis.

--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -1,18 +1,24 @@
 import { Router } from 'express';
 import * as ProductController from '../controllers/ProductController';
-import { authenticateAdmin } from '../middlewares/authMiddleware';
+import { authenticateAdmin } from '../middlewares/authMiddleware'; // Importa o middleware de autenticação de admin
 
-const router = Router();
+const router = Router(); // Cria uma nova instância de roteador
 
 // Rotas protegidas com autenticação de admin
+// Rota para criar um produto (somente admin pode acessar)
 router.post('/products', authenticateAdmin, ProductController.createProductController);
 
+// Rota para atualizar um produto (somente admin pode acessar)
 router.put('/products/:id', authenticateAdmin, ProductController.updateProductController);
 
+// Rota para deletar um produto (somente admin pode acessar)
 router.delete('/products/:id', authenticateAdmin, ProductController.deleteProductController);
 
-// Rotas públicas para visualização
+// Rotas públicas para visualização de produtos
+// Rota para obter um produto pelo ID (acessível publicamente)
 router.get('/products/:id', ProductController.getProductByIdController);
+
+// Rota para obter todos os produtos (acessível publicamente)
 router.get('/products', ProductController.getAllProductsController);
 
 export default router;
